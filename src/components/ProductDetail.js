44f2238bf8b4d31c3216f8952acbdea4fdf53bea
ZaliaMist <73849101+ZaliaMist@users.noreply.github.com>
2021-10-28 02:00:14 -0400

    fixed comments

--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -2,18 +2,9 @@ import React, { useEffect, useState } from "react";
 import { Link, useParams} from "react-router-dom";
 
 function ProductDetail(props) {
-  // const [product, setProduct] = useState({});
-  // When this componen loads, get the Product ID from the url parameter.
-  // Then find the matching product from the productList prop, and assign it to product
 
   const {productId} = useParams() 
-  // console.log(props.productList)
-  // useEffect( () => {
-  //   setProduct( props.productList.find( item => item.id === productId ) );
-  // });
-  // setTimeout(() => {
-  //       setProduct(props.productList)
-  //     }, 1000);
+
   const product = props.productList.find( item => item.id === Number( productId ) )
   
   return (
